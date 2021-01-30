734eaaa05600b311df5146960d899f5b6e844f99
Till Sanders <mail@till-sanders.de>
2021-01-30 22:43:34 +0100

    fix: add FieldSelect and FieldSwitch to main.ts
    Signed-off-by: Till Sanders <mail@till-sanders.de>

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import Styles from './styles/main.scss';
 import BaseButton from './components/base-button/BaseButton.vue';
 import Icon from './components/icon/Icon.vue';
 import FieldInput from './components/fields/input/FieldInput.vue';
+import FieldSelect from './components/fields/select/FieldSelect.vue';
+import FieldSwitch from './components/fields/switch/FieldSwitch.vue';
 
 export {
   Styles,
@@ -12,4 +14,6 @@ export {
   BaseButton,
   Icon,
   FieldInput,
+  FieldSelect,
+  FieldSwitch,
 };
